777818c781751ef34cffae403f72b48873996017
techwulf <t.zach.moore@gmail.com>
2021-08-16 15:02:58 -0600

    complete assignment

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link, useRouteMatch } from 'react-router-dom';
 
 export default function MovieList(props) {
+  const { url } = useRouteMatch();
   return (
     <div className="movie-list">
       {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+        // <Link to={`movies/${movie.id}`}>
+          <MovieDetails key={movie.id} movie={movie} />
+        // </Link>
       ))}
     </div>
   );
@@ -14,14 +18,16 @@ function MovieDetails(props) {
   const { title, director, metascore } = props.movie;
 
   return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
+      <div className="movie-card">
+        <Link to={`movies/${props.movie.id}`}>
+          <h2>{title}</h2>
+          <div className="movie-director">
+            Director: <em>{director}</em>
+          </div>
+          <div className="movie-metascore">
+            Metascore: <strong>{metascore}</strong>
+          </div>
+        </Link>
       </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-    </div>
   );
 }
